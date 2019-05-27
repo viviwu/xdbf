aae68c914f2e66fa14acb469942e295674a4ad8c
viviwu <vivi705@qq.com>
2019-05-27 17:21:25 +0800

    Initial Commit

---
+++ xNotes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  xNotes
+//
+//  Created by vivi wu on 2019/5/27.
+//  Copyright © 2019 vivi wu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
